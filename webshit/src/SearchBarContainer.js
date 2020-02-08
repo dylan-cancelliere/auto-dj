0ca7b66c9c3d75b1edfb9223d1fe686f9ebddb78
dylan-cancelliere <dycancel@gmail.com>
2020-02-08 14:14:55 -0500

    Created mainpage in react from mockup

---
+++ webshit/src/SearchBarContainer.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import "./SearchBarContainer.css";
+
+class SearchBarContainer extends Component{
+    render(){
+        return(
+            <input type="text" placeholder="Enter a code..."/>
+        );
+    }
+}
+
+export default SearchBarContainer;
