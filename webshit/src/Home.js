fc4c85dfcc1c9d7da04b21edbb80c43bf2f2b6d2
dylan-cancelliere <dycancel@gmail.com>
2020-02-15 20:17:10 -0500

    Implemented Routing for navigation between pages

--- webshit/src/Home.js
+++ webshit/src/Home.js
@@ -1,12 +1,24 @@
 import SearchBarContainer from "./SearchBarContainer";
 import CreateRoomContainer from "./CreateRoomContainer";
 import React, {Component} from "react";
+import "./Home.css";
 
 class Home extends Component{
     render(){
         return(
-
-        )
+            <div>
+                <div id="toptext">
+                    <h1>Join a Room</h1>
+                </div>
+                <div id="bottomtext">
+                    <h1>or</h1>
+                </div>
+                <div>
+                    <SearchBarContainer/>
+                    <CreateRoomContainer/>
+                </div>
+            </div>
+        );
     }
 }
 
--- webshit/src/Main.js
+++ webshit/src/Main.js
@@ -4,19 +4,23 @@ import {
     NavLink,
     HashRouter
 } from "react-router-dom";
-import Home from "./Home"
+import Home from "./Home";
+import Lobby from "./Lobby";
+import "./Main.css"
 
 class Main extends Component{
     render(){
         return(
             <HashRouter>
                 <div>
-                    <h1>Simple SPA</h1>
+                    <div id="title"><h1>AutoDJ</h1></div>
                     <ul className="header">
                         <li><NavLink exact to="/">Home</NavLink></li>
+                        <li><NavLink to="/lobby">Lobby</NavLink></li>
                     </ul>
                     <div className="content">
                         <Route exact path="/" component={Home}/>
+                        <Route path="/lobby" component={Lobby}/>
                     </div>
                 </div>
             </HashRouter>
