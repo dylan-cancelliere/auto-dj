ec53e67930b8a9e8761ac06d7bd48bfa984f591d
dylan-cancelliere <dycancel@gmail.com>
2020-04-14 22:51:59 -0400

    Boilerplate code for Spotify WebPlayer SDK

--- client/src/components/Lobby/WebPlayer.js
+++ client/src/components/Lobby/WebPlayer.js
@@ -4,7 +4,34 @@ class WebPlayer extends Component{
     render(){
         return(
             <div className="web-player">
-                <script src="https://sdk.scdn.co/spotify-player.js"></script>
+                <script src="https://sdk.scdn.co/spotify-player.js">
+                </script>
+                {window.onSpotifyWebPlaybackSDKReady = () => {
+                    const token = 'BQChHZ7jKPBg0BOV5koUpbJvwOgF7ye-KtjUdOhVjo4JJQ85nGybswb--kwPagI2Anj2Il6l0e3DegsQQZSk8vVPcgOPYPXz3RfPASMnsrx6gI4ks7KLCs2VZKSuWud4npeoQbwtayf6O9gn_XanA4kjzl4qEEii5yx9sP2wtLeRUIVbr2CN1c4BtmBUcw'; //Auth Token
+                    const player = new Spotify.Player({
+                        name: 'Web Playback SDK Quick Start Player',
+                        getOAuthToken: cb => {cb(token);}
+                    });
+
+                    //Error handling
+                    player.addListener('initialization_error', ({message}) => {
+                        console.error(message); });
+                    player.addListener('authentication_error', ({message}) => {
+                        console.error(message); });
+                    player.addListener('account_error', ({message}) => {console.error(message); });
+                    player.addListener('playback_error', ({message}) => {console.error(message); });
+
+                    //Playback status updates
+                    player.addListener('player_state_changed', state => {console.log(state); });
+
+                    //Ready
+                    player.addListener('ready', ({device_id}) => {
+                        console.log('Ready with Device ID', device_id);
+                    });
+
+                    //Connect to the player
+                    player.connect();
+                }}
             </div>
         )
     }
