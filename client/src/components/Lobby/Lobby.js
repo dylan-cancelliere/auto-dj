9b8c1e68a961623240f85791cc317e1a9e0c076a
dylan-cancelliere <dycancel@gmail.com>
2020-04-15 07:56:07 -0400

    Dynamically pass oAuth token through state

--- client/src/components/Lobby/Lobby.js
+++ client/src/components/Lobby/Lobby.js
@@ -4,10 +4,17 @@ import Room from '../Rooms/Rooms';
 import WebPlayer from "./WebPlayer";
 
 class Lobby extends Component {
+    constructor(props){
+        super(props);
+        this.state = {auth_token: "", auth: false};
+    }
     render() {
+        if (this.state.auth === false && window.location.hash.slice(8, 20) === "access_token"){
+            this.setState({auth_token: window.location.hash.slice(21), auth: true})
+        }
         return (
             <div className="web-player-container">
-                <WebPlayer/>
+                <WebPlayer auth={this.state.auth} auth_token={this.state.auth_token}/>
             </div>
 
         )
--- client/src/components/Lobby/WebPlayer.js
+++ client/src/components/Lobby/WebPlayer.js
@@ -2,13 +2,11 @@ import React, {Component} from "react";
 import "./WebPlayer.css";
 import SpotifyPlayer from 'react-spotify-web-playback';
 
-const tempAuthToken = 'BQCepsNdej0RM5AlKBtF8UnmOei4Fti7cfQpA8TGRQImfdfKev-_OWLc6Xx42JYwg3wGv-KPNoKSMk1YG4Zp5Wowey_KKv9ecDRxYya0oZoObM6LwzWSwz03Isd-t3Wqb7xs_gwL4jS63qOeoaKF3jzLCFC-9whDZiTPVFtr_mO-5EHwrcfnAjRA4xie_Q&refresh_token=AQDH-DzHJWWSg9uj8RON483kki53wvph7KJmfxkOhOs54AGzbIk3BnySbYaIaeNzZga6vFz7h7YFmBY6RrAz2YldeMDLsVc2gKC8ASMkaAZq0W9tIbiVIAAG5vSAEyvt7hI';
-
 class WebPlayer extends Component{
     render(){
         return(
             <div className="web-player">
-                <SpotifyPlayer token={tempAuthToken}
+                <SpotifyPlayer token={this.props.auth_token}
                                autoPlay="true"
                                name="AutoDJ"
                                styles={{
